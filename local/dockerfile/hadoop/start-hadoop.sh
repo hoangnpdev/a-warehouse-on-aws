ccbdca7443151b932a1e5503b84e2aac9aea864e
hoangnp <hoang.np193@gmail.com>
2025-04-21 17:34:01 +0700

    update yarn, hdfs, compose configs

--- local/dockerfile/hadoop/start-hadoop.sh
+++ local/dockerfile/hadoop/start-hadoop.sh
@@ -1,6 +1,9 @@
 export JAVA_HOME=/usr/local/java/java-se-8u44-ri/
 export HADOOP_HOME=/usr/local/hadoop/hadoop-3.4.1/etc/hadoop/
+export HADOOP_CONF_DIR=${HADOOP_HOME}/usr/local/hadoop/hadoop-3.4.1/etc/hadoop/
 
 $HADOOP_HOME/bin/hdfs --daemon start "$HDFS_NODE_TYPE"
 
+$HADOOP_HOME/bin/yarn --daemon start "$YARN_NODE_TYPE"
 
+wait -n
