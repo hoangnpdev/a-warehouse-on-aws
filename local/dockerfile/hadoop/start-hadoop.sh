6cbbac2e5199e7da390221c2eb58636b7210f300
hoangnp <hoang.np193@gmail.com>
2025-04-23 16:04:50 +0700

    update hadoop config

--- local/dockerfile/hadoop/start-hadoop.sh
+++ local/dockerfile/hadoop/start-hadoop.sh
@@ -1,9 +1,14 @@
-export JAVA_HOME=/usr/local/java/java-se-8u44-ri/
-export HADOOP_HOME=/usr/local/hadoop/hadoop-3.4.1/etc/hadoop/
-export HADOOP_CONF_DIR=${HADOOP_HOME}/usr/local/hadoop/hadoop-3.4.1/etc/hadoop/
+export JAVA_HOME=/usr/local/java/jdk1.8.0_291
+export HADOOP_HOME=/usr/local/hadoop/hadoop-3.4.1
+export HADOOP_CONF_DIR=${HADOOP_HOME}/etc/hadoop
+
+if [ "$HDFS_NODE_TYPE" = "namenode" ]
+then
+  $HADOOP_HOME/bin/hdfs namenode -format
+fi
 
 $HADOOP_HOME/bin/hdfs --daemon start "$HDFS_NODE_TYPE"
 
 $HADOOP_HOME/bin/yarn --daemon start "$YARN_NODE_TYPE"
 
-wait -n
+sleep infinity
