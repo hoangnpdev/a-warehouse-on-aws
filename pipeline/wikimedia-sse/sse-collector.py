8e99dae9e3ce586b3e5659d5f6bc002ec75a69a6
hoangnp <hoang.np193@gmail.com>
2025-04-29 18:10:58 +0700

    sse pipeline

--- pipeline/wikimedia-sse/sse-collector.py
+++ pipeline/wikimedia-sse/sse-collector.py
@@ -1,5 +1,37 @@
-import requests
+import argparse
+import json
+from requests_sse import EvenSource
 
 
+def collect_wiki_recent_change():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--date", dest="date")
+    args = parser.parse_args()
 
-requests.get()
+    date = args.date
+    # todo format date
+    url = f'https//stream.wikimedia.org/v2/stream/recentchange?since={date}'
+    with EvenSource(url) as stream:
+        for event in stream:
+            current_batch= '' # timestamp each 5 minutes
+            data = []
+            if event.type == 'message':
+                # todo append event to local file each 5 minutes
+                change = json.loads(event.data)
+                if change.timestamp > batch_period:
+                    save_batch(data, f'{current_batch}.csv')
+                    data = []
+                    current_batch = 'next batch' # todo next batch
+                if change.timestamp < date + 24 hour:
+                    data.append(extract_change(change))
+
+                if change.timestamp > date + 24 hour + 5 minutes:
+                    break
+
+def extract_change(change):
+    return
+
+def save_batch(data, path):
+    return
+
+collect_wiki_recent_change()
